0cf4465e784e3b03679aa9e818344d635d94788a
Lucas Emiliano <174640759+Mirante5@users.noreply.github.com>
2025-02-27 10:50:00 -0300

    Update OUVIDORIA - ARQUIVAR.user.js

--- OUVIDORIA - ARQUIVAR.user.js
+++ OUVIDORIA - ARQUIVAR.user.js
@@ -38,22 +38,22 @@
 
             // Adicionar opções ao dropdown
             const justificativas = [
-                { text: '',
+                { text: 'Selecione uma justificativa...',
                  value: '' },
-                { text: '',
-                 value: ``
+                { text: 'Duplicidade',
+                 value: `Prezado(a) Senhor(a),\n\nConsiderando que sua manifestação está em duplicidade ou é semelhante a outra tratada por esta Ouvidoria, realizamos o arquivamento do presente protocolo e sugerimos que o acompanhamento de sua manifestação seja feito por NUP (NUP).\n\nAgradecemos a sua colaboração e colocamo-nos à disposição sempre que desejar falar com a Ouvidoria do Ministério da Educação.\n\nAtenciosamente,\nOuvidoria do Ministério da Educação.`
                 },
-                { text: '',
-                 value: ''
+                { text: 'Comunicado sem materialidade',
+                 value: 'Prezado(a) Senhor(a),\n\nConsiderando que os fatos elencados não representam elementos mínimos descritivos de irregularidade ou não contêm indícios que permitam à administração pública chegar a tais elementos e, ainda, que na modalidade de comunicação de irregularidade não há a possibilidade de complementação de informação prevista no art. 16 da Portaria CGU nº 116, de 2024, arquivamos o presente protocolo. \n\nAtenciosamente, \n\nOuvidoria do Ministério da Educação \n\nAvaliação Ouvidoria \n\nSua opinião é fundamental para nós! Queremos convidar você a compartilhar sua experiência com a Ouvidoria do MEC (OUV/MEC) e as unidades do Ministério da Educação. \n\nSua participação nos ajudará a melhorar nossos serviços e garantir que continuemos atendendo às suas necessidades da melhor forma possível. \n\nNos avalie diretamente pela Plataforma Fala.BR, leva apenas alguns minutos e seu feedback será extremamente valioso. \n\nContamos com você para fazer a diferença!'
                 },
-                { text: '',
-                 value: ''
+                { text: 'Falta de urbanidade',
+                 value: 'Prezado(a) Senhor(a), \n\nA Ouvidoria do Ministério da Educação esclarece que, de acordo com a Lei nº 13.460, de 26 de junho de 2017, art. 8°, inciso I, são deveres do usuário “utilizar adequadamente os serviços, procedendo com urbanidade e boa-fé”. Dessa forma, os termos utilizados inviabilizam a análise de sua mensagem e, por essa razão, ela foi encerrada nesta Ouvidoria. \n\nColocamo-nos à sua disposição sempre que desejar falar com a Ouvidoria do Ministério da Educação. \n\nAtenciosamente, \n\nOuvidoria do Ministério da Educação \n\nACESSE: https: https://www.gov.br/mec/pt-br/canais_atendimento/ouvidoria \n\nAvaliação Ouvidoria \n\nQueremos convidar você a compartilhar sua experiência com a Ouvidoria do MEC (OUV/MEC) e as unidades do Ministério da Educação. \n\nSua participação nos ajudará a melhorar nossos serviços e garantir que continuemos atendendo às suas necessidades da melhor forma possível. \n\nNos avalie diretamente pela Plataforma Fala.BR, leva apenas alguns minutos e seu feedback será extremamente valioso. \n\nContamos com você para fazer a diferença! '
                 },
-                { text: '',
-                 value: ''
+                { text: 'Cópia para diversos órgãos',
+                 value: 'Prezado(a) Senhor(a), \n\nVerificamos que consta em sua manifestação a informação de envio de cópia para diversas instituições, as quais possuem competências para análise do assunto. Dessa forma, a Ouvidoria do Ministério da Educação não é o principal destinatário, razão pela qual encerramos sua demanda no âmbito desta Ouvidoria. \n\nColocamo-nos à sua disposição sempre que desejar falar com a Ouvidoria do Ministério da Educação. \n\nAtenciosamente, \n\nOuvidoria do Ministério da Educação \n\nACESSE: https: https://www.gov.br/mec/pt-br/canais_atendimento/ouvidoria \n\nAvaliação Ouvidoria \n\nQueremos convidar você a compartilhar sua experiência com a Ouvidoria do MEC (OUV/MEC) e as unidades do Ministério da Educação. \n\nSua participação nos ajudará a melhorar nossos serviços e garantir que continuemos atendendo às suas necessidades da melhor forma possível. \n\nNos avalie diretamente pela Plataforma Fala.BR, leva apenas alguns minutos e seu feedback será extremamente valioso. \n\nContamos com você para fazer a diferença! '
                 },
-                { text: '',
-                 value: '' }
+                { text: 'Perda do objeto',
+                 value: 'Prezado(a) Senhor(a), \n\nInformamos que em razão de XXXXXXXXXXXXX houve a perda do objeto de sua manifestação. Assim, encerramos a sua manifestação nesta Ouvidoria. \n\nColocamo-nos à sua disposição sempre que desejar falar com a Ouvidoria do Ministério da Educação. \n\nAtenciosamente, \n\nOuvidoria do Ministério da Educação \n\nACESSE: https: https://www.gov.br/mec/pt-br/canais_atendimento/ouvidoria \n\nAvaliação Ouvidoria \n\nQueremos convidar você a compartilhar sua experiência com a Ouvidoria do MEC (OUV/MEC) e as unidades do Ministério da Educação. \n\nSua participação nos ajudará a melhorar nossos serviços e garantir que continuemos atendendo às suas necessidades da melhor forma possível.  \n\nNos avalie diretamente pela Plataforma Fala.BR, leva apenas alguns minutos e seu feedback será extremamente valioso.  \n\nContamos com você para fazer a diferença! ' }
             ];
 
             // Popula o dropdown com as opções
@@ -84,4 +84,3 @@
         }
     });
 })();
-
