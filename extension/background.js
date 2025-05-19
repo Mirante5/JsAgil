133012bd7633204cb3fc04471eeb4ca1c38a3e2b
Lucas Emiliano <174640759+Mirante5@users.noreply.github.com>
2025-05-19 11:34:35 -0300

    Delete extension directory

--- extension/background.js
+++
@@ -1,6 +0,0 @@
-// background.js
-// Este service worker fica “ocioso” na maior parte do tempo.
-// Se você precisar de lógica em background (alarmes, notificações, etc),
-// pode adicionar aqui.
-
-//, neste exemplo, deixamos vazio:
